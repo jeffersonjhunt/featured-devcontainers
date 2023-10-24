3d68078ee029a3288960a74626291e7b499e55ce
Jefferson Hunt <jeffersonjhunt@gmail.com>
2023-10-23 19:18:49 -0500

    add cpp

---
+++ src/cpp/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://github.com/devcontainers/features/blob/main/LICENSE for license information.
+#-------------------------------------------------------------------------------------------------------------------------
+#
+# Docs: https://github.com/devcontainers/features/tree/main/src/common-utils
+# Maintainer: The Dev Container spec maintainers
+
+set -e
+
+INSTALL_BOOST="${INSTALLBOOST:-"true"}"
+INSTALL_CMAKE="${INSTALLCMAKE:-"true"}"
+INSTALL_FMT="${INSTALLFMT:-"true"}"
+
+MARKER_FILE="/usr/local/etc/vscode-dev-containers/cpp"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# If we're using Alpine, install bash before executing
+. /etc/os-release
+if [ "${ID}" = "alpine" ]; then
+    apk add --no-cache bash
+fi
+
+exec /bin/bash "$(dirname $0)/main.sh" "$@"
+exit $?
